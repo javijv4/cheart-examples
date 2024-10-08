a765275c9e288a859244290db8dc91104aab8fd7
jilberto@umich.edu <jilberto@umich.edu>
2024-10-08 15:23:13 -0400

    fixing example3

--- example3_simple_stretch_fibers/visualize_results.py
+++ example3_simple_stretch_fibers/visualize_results.py
@@ -17,26 +17,12 @@ map_quad = chio.map_between_meshes(mesh_quad, mesh)
 fibers = chio.read_dfile('out/fibers-1.D')
 mesh.point_data['fibers'] = fibers[:,0:3]
 
-strain = np.zeros([10, mesh.points.shape[0]])
-for i in range(1, 11):
-    fibstrain = chio.read_dfile(f'out/FibStrain-{i}.D')
-    mesh.point_data['fibstrain'] = fibstrain
-    strain[i-1] = fibstrain
-
-diff_strain = np.diff(strain, axis=0)/0.1
-diff_strain = np.concatenate((np.zeros([1, mesh.points.shape[0]]), diff_strain), axis=0)
-
 for i in range(1, 11):
     disp = chio.read_dfile(f'out/Disp-{i}.D')
     mesh.point_data['disp'] = disp[map_quad]
 
-    fibstrain = chio.read_dfile(f'out/FibStrain-{i}.D')
-    mesh.point_data['fibstrain'] = fibstrain
-    mesh.point_data['diff_strain'] = diff_strain[i-1]
-
-    fibstrainrate = chio.read_dfile(f'out/FibStrainRate-{i}.D')
-    mesh.point_data['fibstrainrate'] = fibstrainrate
-
+    fibstretch = chio.read_dfile(f'out/FibStretch-{i}.D')
+    mesh.point_data['fibstretch'] = fibstretch
 
     io.write(f'out/results-{i}.vtu', mesh)
 
